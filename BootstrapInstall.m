9f2408041227ae368b541432e89b25870d458f9b
jkuczm <Jakub.Kuczmarski@gmail.com>
2014-06-30 00:19:47 +0200

    Use BootstrapInstaller package

--- BootstrapInstall.m
+++ BootstrapInstall.m
@@ -1,55 +1,33 @@
 (* ::Package:: *)
 
-Module[
-	{$PIImportResult}
-	,
-	
-	Quiet[
-		$PIImportResult = Needs["ProjectInstaller`"],
-		{Get::noopen, Needs::nocont}
-	];
-	
-	If[$PIImportResult === $Failed,
-		Print["ProjectInstaller not found, installing it:"];
-		Print @ Import[
-			"https://raw.github.com/lshifr/ProjectInstaller/master/BootstrapInstall.m"
-		];
-		$PIImportResult = Needs["ProjectInstaller`"];
-	];
+Get["https://raw.githubusercontent.com/jkuczm/MathematicaBootstrapInstaller/v0.1.0/BootstrapInstaller.m"]
 
-	If[$PIImportResult === $Failed,
-		Print[
-			"Unable to load ProjectInstaller.\n",
-			"Please ",
+
+BootstrapInstall[
+	"MUnitExtras",
+	"https://github.com/jkuczm/MUnitExtras/releases/download/v0.1.1/MUnitExtras.zip"
+	,
+	{
+		#1
+		,
+		"https://github.com/jkuczm/Mathematica" <> #1 <>
+		"/releases/download/v" <> #2 <> "/" <> #1 <> ".zip"
+	}& @@@ {
+		"EvaluationUtilities" -> "0.1.0",
+		"MessagesUtilities" -> "0.1.0",
+		"OptionsUtilities" -> "0.1.0",
+		"PatternUtilities" -> "0.1.0",
+		"ProtectionUtilities" -> "0.1.0",
+		"StringUtilities" -> "0.1.0"
+	}
+	,
+	"AdditionalFailureMessage" -> 
+		Sequence[
+			"You can ", 
 			Hyperlink[
-				"install MUnitExtras package manually",
+				"install MUnitExtras package manually", 
 				"https://github.com/jkuczm/MUnitExtras#manual-installation"
 			],
-			".\n",
-			"We would be grateful for ",
-			Hyperlink[
-				"reporting this issue",
-				"https://github.com/jkuczm/MUnitExtras/issues"
-			],
 			"."
-		];
-	(* else *),
-		Print["Installing dependencies:"];
-		(Print @ ProjectInstaller`ProjectInstall @ URL[
-			"https://github.com/jkuczm/Mathematica" <> #1 <>
-			"/releases/download/v" <> #2 <> "/" <> #1 <> ".zip"
-		])& @@@ {
-			"EvaluationUtilities" -> "0.1.0",
-			"MessagesUtilities" -> "0.1.0",
-			"OptionsUtilities" -> "0.1.0",
-			"PatternUtilities" -> "0.1.0",
-			"ProtectionUtilities" -> "0.1.0",
-			"StringUtilities" -> "0.1.0"
-		};
-		
-		Print["Installing MUnitExtras:"];
-		Print @ ProjectInstaller`ProjectInstall @ URL[
-			"https://github.com/jkuczm/MUnitExtras/releases/download/v0.1.1/MUnitExtras.zip"
-		];
-	];
+		]
 ]
